c1eb45e4a79e230c3c8d049db173439c9f5b144b
joshua-faylor <jfayl03@byu.edu>
2025-12-04 14:58:59 -0700

    fixed the connection to the database

--- index.js
+++ index.js
@@ -56,6 +56,7 @@ const knex = require("knex")({
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE,
     port: process.env.POSTGRES_PORT,
+    ssl: { rejectUnauthorized: false }
   },
   wrapIdentifier: (value, origImpl) => origImpl(value.toLowerCase()),
 });
